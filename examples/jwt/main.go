2e96f5e3fc9686595cb614a035c3d70bb67ca775
hoenirvili <hoenirvili@gmail.com>
2019-06-23 00:19:16 +0300

    Add jwt token validation example
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

---
+++ examples/jwt/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/hoenirvili/rester"
+	"github.com/hoenirvili/rester/request"
+	"github.com/hoenirvili/rester/resource"
+	"github.com/hoenirvili/rester/response"
+	"github.com/hoenirvili/rester/route"
+	"github.com/hoenirvili/rester/token"
+)
+
+type jsonResponse struct {
+	Message string `json:"message"`
+}
+
+type root struct{}
+
+func (r *root) index(req request.Request) resource.Response {
+	return response.Response{Payload: &jsonResponse{"Hello World !"}}
+}
+
+func (r *root) Routes() route.Routes {
+	return route.Routes{{
+		URL:     "/",
+		Method:  resource.Get,
+		Handler: r.index,
+	}}
+}
+
+var pub = []byte(`
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6X5gMNrebNra73WI7jIX
+jQSTiBEvI79hMlT+b2qrxDKtq0wY8CaNkr9k5sFyzE/ImGo4NHz3rArkKIQU5uSy
+Y0xjJvN0uhqmNGv3F+yXf/ak9BIyiq6TM45j15KVbw+dLHSZJhN8ZZ96C36dOyB2
+JPInjkU+pMzJr/Dp6KBL5dDqYqSQzmojb0KxyNyn1VGvurG5e3TFCH4377bg+H0Z
+C48AKkhokFvhhI7MJpFhfoQaGqAgg3kOguWOpngDcaTwschjwZ2rW9/qguf6iSsG
+77+5RyJrlkZbbpaQl83gEs2EMzykmhzfhDJWXsEA0+HH4ns8XDgqlIodIkTmf2/5
+lQIDAQAB
+-----END PUBLIC KEY-----
+`)[1:]
+
+
+func main() {
+	key, err := jwt.ParseRSAPublicKeyFromPEM(pub)
+	if err != nil {
+		panic(err)
+	}
+	token := token.NewJWT(key)
+	rester := rester.New(
+		rester.WithTokenValidator(token),
+	)
+	rester.Resource("/", new(root))
+	if err := http.ListenAndServe(":8080", rester); err != nil {
+		panic(err)
+	}
+}
