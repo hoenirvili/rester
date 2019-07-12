0c76a9edbc3d9e3eb4296a3693e45d8126dca79c
hoenirvili <hoenirvili@gmail.com>
2019-07-12 15:52:41 +0300

    Add more unit tests for the rest spec
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- rester.go
+++ rester.go
@@ -171,15 +171,12 @@ func serveFiles(r chi.Router, path string, root http.FileSystem) {
 	if strings.ContainsAny(path, "{}*") {
 		panic("serving files does not permit URL parameters")
 	}
-
 	fs := http.StripPrefix(path, http.FileServer(root))
-
 	if path != "/" && path[len(path)-1] != '/' {
 		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
 		path += "/"
 	}
 	path += "*"
-
 	r.Get(path, http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			fs.ServeHTTP(w, r)
@@ -199,6 +196,9 @@ func (r *Rester) Static(dir string) {
 // dispatching requests
 func (r *Rester) Build() {
 	r.root.Group(func(g chi.Router) {
+		if r.options.version == "" {
+			r.options.version = "/"
+		}
 		router := chi.NewRouter()
 		g.Mount(r.options.version, router)
 		router.NotFound(r.config.notfound)
@@ -256,6 +256,9 @@ func (r *Rester) Resource(base string, resource Resource) {
 }
 
 func httphandler(h handler.Handler, pairs query.Pairs) http.HandlerFunc {
+	if h == nil {
+		return nil
+	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		request := request.New(r, pairs)
 		response := h(request)
--- rester_test.go
+++ rester_test.go
@@ -1,6 +1,7 @@
 package rester_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -80,15 +81,15 @@ type payload struct {
 var testpayload = payload{"test"}
 
 func index(request.Request) resource.Response {
-	return response.Response{Payload: &testpayload}
+	return response.Payload(&testpayload)
 }
 
 type testResource struct{}
 
 func (t *testResource) Routes() route.Routes {
 	return route.Routes{{
-		Method:  resource.Post,
 		URL:     "/test",
+		Method:  resource.Post,
 		Handler: index,
 	}}
 }
@@ -103,6 +104,7 @@ func (r *resterSuite) SetupSuite() {
 	r.rester.NotFound(handler.Handler(notfound))
 	r.rester.MethodNotAllowed(handler.Handler(methodnotallowed))
 	r.rester.Resource("/", new(testResource))
+	r.rester.Build()
 }
 
 func (r resterSuite) TestNotFound() {
@@ -139,6 +141,26 @@ func (r resterSuite) TestMethodNotAllowed() {
 	r.require.Equal(http.StatusMethodNotAllowed, resp.StatusCode)
 	r.require.Contains(resp.Header, "Testkey")
 	r.require.Equal(header["Testkey"], resp.Header["Testkey"])
+}
+
+func TestWithVersioning(t *testing.T) {
+	require := require.New(t)
+	rester := rester.New(rester.WithVersioning("v1"))
+	rester.Resource("/", new(testResource))
+	rester.Build()
+	server := httptest.NewServer(rester)
+	defer server.Close()
+
+	resp, err := http.Post(server.URL+"/v1/test", "application/json", &bytes.Buffer{})
+	require.NoError(err)
+	require.NotEmpty(resp)
+	defer resp.Body.Close()
+
+	require.Equal(http.StatusOK, resp.StatusCode)
+	payload := payload{}
+	err = json.NewDecoder(resp.Body).Decode(&payload)
+	require.NoError(err)
+	require.Equal(testpayload, payload)
 }
 
 func TestResterSuite(t *testing.T) {
