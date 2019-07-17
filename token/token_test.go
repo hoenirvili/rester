97e4348d906d3c5d44edc57c3067015b44c7e420
hoenirvili <hoenirvili@gmail.com>
2019-07-18 00:38:42 +0300

    Add token unit tests
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- token/token_test.go
+++ token/token_test.go
@@ -1,11 +1,15 @@
 package token_test
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
 	gojwt "github.com/dgrijalva/jwt-go"
-	"github.com/hoenirvili/rester/token"
 	"github.com/stretchr/testify/require"
+
+	"github.com/hoenirvili/rester/token"
 )
 
 func TestNewJWT(t *testing.T) {
@@ -22,27 +26,60 @@ func jwt(r *require.Assertions) *token.JWT {
 	return token
 }
 
-func TestVerifyToken(t *testing.T) {
-	require := require.New(t)
-	_ = require
+func tokenString(r *require.Assertions) string {
+	private, err := gojwt.ParseRSAPrivateKeyFromPEM(priv)
+	r.NoError(err)
+	token := gojwt.NewWithClaims(gojwt.SigningMethodRS256, gojwt.MapClaims{
+		"exp": time.Now().Add(time.Hour * 1).Unix(),
+	})
+	stoken, err := token.SignedString(private)
+	r.NoError(err)
+	return stoken
 }
 
-// type jwtSuite struct {
-// 	suite.Suite
-// 	token *token.JWT
-// }
-//
-// func (s *jwtSuite) SetupTest() {
-// 	require := s.Require()
-// 	jwt := token.NewJWT(nil)
-// 	require.NotEmpty(jwt)
-//
-// }
-//
-// func (s *jwtSuite) TearDownTest() {
-//
-// }
-//
-// func TestJWTToken(t *testing.T) {
-// 	suite.Run(t, new(jwtSuite))
-// }
+func tokenStringWithoutAnyClaims(r *require.Assertions) string {
+	private, err := gojwt.ParseRSAPrivateKeyFromPEM(priv)
+	r.NoError(err)
+	token := gojwt.New(gojwt.SigningMethodRS256)
+	stoken, err := token.SignedString(private)
+	r.NoError(err)
+	return stoken
+}
+
+func tokenStringExpired(r *require.Assertions) string {
+	private, err := gojwt.ParseRSAPrivateKeyFromPEM(priv)
+	r.NoError(err)
+	token := gojwt.NewWithClaims(gojwt.SigningMethodRS256, gojwt.MapClaims{
+		"exp": time.Now().Add(time.Hour * -1).Unix(),
+	})
+	stoken, err := token.SignedString(private)
+	r.NoError(err)
+	return stoken
+}
+
+func TestVerifyToken(t *testing.T) {
+	require := require.New(t)
+	token := jwt(require)
+	tokenString := tokenString(require)
+	err := token.Verify(&http.Request{
+		Header: http.Header{"Authorization": []string{fmt.Sprintf("Bearer %s", tokenString)}},
+	})
+	require.NoError(err)
+}
+
+func TestVerifyTokenWithBadInput(t *testing.T) {
+	require := require.New(t)
+	inputs := []string{
+		fmt.Sprintf("Bearer %s", "badtoken"),
+		"Bearer", "", "sfhjiuasdhgiusdfsdfa",
+		fmt.Sprintf("Bearer %s", tokenStringExpired(require)),
+		fmt.Sprintf("Bearer %s", tokenStringWithoutAnyClaims(require)),
+	}
+	for _, input := range inputs {
+		token := jwt(require)
+		err := token.Verify(&http.Request{
+			Header: http.Header{"Authorization": []string{input}},
+		})
+		require.Error(err)
+	}
+}
