9960309370ef4fa40f0f2087077b2ee70b347bdb
hoenirvili <hoenirvili@gmail.com>
2019-07-18 15:06:35 +0300

    Finish writing unit tests for response package
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- response/response_test.go
+++ response/response_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/hoenirvili/rester/permission"
 	"github.com/hoenirvili/rester/response"
 )
 
@@ -51,5 +52,127 @@ func TestHeaders(t *testing.T) {
 	}
 
 	require.Equal(t, resp, p)
+}
+
+func TestInternalError(t *testing.T) {
+	p := response.InternalError("test")
+	resp := &response.Response{
+		Error:      response.Error("test"),
+		StatusCode: http.StatusInternalServerError,
+	}
+	require.Equal(t, resp, p)
+}
+
+func TestNotFound(t *testing.T) {
+	p := response.NotFound("test")
+	resp := &response.Response{
+		Error:      response.Error("test"),
+		StatusCode: http.StatusNotFound,
+	}
+	require.Equal(t, resp, p)
+}
+
+func TestMethodNotAllowed(t *testing.T) {
+	p := response.MethodNotAllowed("test")
+	resp := &response.Response{
+		Error:      response.Error("test"),
+		StatusCode: http.StatusMethodNotAllowed,
+	}
+	require.Equal(t, resp, p)
+}
+
+func TestBadRequest(t *testing.T) {
+	p := response.BadRequest("test")
+	resp := &response.Response{
+		Error:      response.Error("test"),
+		StatusCode: http.StatusBadRequest,
+	}
+	require.Equal(t, resp, p)
+}
+
+func TestUnauthorized(t *testing.T) {
+	p := response.Unauthorized("test")
+	resp := &response.Response{
+		Error:      response.Error("test"),
+		StatusCode: http.StatusUnauthorized,
+	}
+	require.Equal(t, resp, p)
+}
+
+func TestWithPermission(t *testing.T) {
+	p := response.WithPermission("test", permission.Anonymous)
+	resp := &response.Response{Payload: "test"}
+	require.Equal(t, resp.Payload, p.Payload)
+}
+
+type responseWriter struct {
+	header     http.Header
+	statusCode int
+	p          []byte
+}
+
+func newResponseWriter() *responseWriter {
+	return &responseWriter{header: make(http.Header)}
+}
+
+func (r *responseWriter) StatusCode() int        { return r.statusCode }
+func (r *responseWriter) Data() []byte           { return r.p }
+func (r *responseWriter) Header() http.Header    { return r.header }
+func (r *responseWriter) WriteHeader(status int) { r.statusCode = status }
+func (r *responseWriter) Write(p []byte) (int, error) {
+	r.p = append(r.p, p...)
+	return len(p), nil
+}
+
+func TestRender(t *testing.T) {
+	responses := map[*response.Response]*responseWriter{
+		response.NotFound("test"):                         newResponseWriter(),
+		response.Payload("test"):                          newResponseWriter(),
+		response.Ok():                                     newResponseWriter(),
+		&response.Response{Error: response.Error("test")}: newResponseWriter(),
+	}
+	for response, writer := range responses {
+		response.Render(writer)
+		require.Equal(t, writer.Header(), http.Header{
+			"Content-Type": []string{"application/json"},
+		})
+	}
+}
+
+func TestRenderContent(t *testing.T) {
+	r := response.NotFound("test")
+	w := newResponseWriter()
+	r.Render(w)
+	require.Equal(t, string(w.Data()), `{"error":"test"}`+"\n")
+}
+
+func TestRenderWithHeader(t *testing.T) {
+	r := response.Header("String-Length", "Custom header")
+	w := newResponseWriter()
+	r.Render(w)
+	require.Equal(t, w.Header(), http.Header{
+		"String-Length": []string{"Custom header"},
+		"Content-Type":  []string{"application/json"},
+	})
+}
+
+type jsonResponse struct {
+	Message string `json:"message,omitempty"`
+}
+
+func (j *jsonResponse) Payload(p permission.Permissions) interface{} {
+	if p == permission.Admin {
+		j.Message = ""
+		return j
+	}
+	return j
+}
+
+func TestRenderWithPermissions(t *testing.T) {
+	payload := &jsonResponse{"test"}
+	r := response.WithPermission(payload, permission.Admin)
+	w := newResponseWriter()
+	r.Render(w)
+	require.Equal(t, string(w.Data()), "{}\n")
 
 }
